ece6bc82f5fff76266585629b11c2b1b848b89d3
Charles Vaghetti <63205923+v4gh3tt1@users.noreply.github.com>
2020-12-20 14:05:11 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WS_JavaCursos
+ Basico de Java
---
+++ StreamApi/StreamAPI/src/streams/ImprimindoObjetos.java
@@ -0,0 +1,42 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ImprimindoObjetos {
+	
+	public static void main(String[] args) {
+		
+		List<String> aprovados = Arrays.asList("Lu", "Gui", "Luca", "Ana");
+		
+		//forma mais simples de percorrer os elementos com laço for
+		for (int i = 0; i < aprovados.size(); i++) {
+			System.out.println(aprovados.get(i));
+			
+		}
+		System.out.println("\n*-*-*-*-*-*-*-*-*");
+		
+		// usando o foreach
+		for(String nome: aprovados) {
+			System.out.println(nome);
+		}
+		
+		System.out.println("\n*-*-*-*-*-*-*-*-*");
+		
+		// usando o Iterator, quando nao sabe a quantidade certa, usar while
+		Iterator<String> it = aprovados.iterator();
+		
+		while (it.hasNext()){
+			System.out.println(it.next());
+		}
+		
+		// usando strem ->laço interno
+		System.out.println("\n*-*-*-*-*-*-*-*-*");
+		Stream<String> st = aprovados.stream();
+		st.forEach(System.out::println);
+		
+	}
+
+}
